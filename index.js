a5b8ffd7c15012e3491bac9c2e41903a6eb261e1
hrl7 <hrlclb@gmail.com>
2016-11-14 23:49:10 +0900

    babelified

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
 "use strict";
-const PORT = 4422;
-const path = require('path');
+// const PORT = 4422;
+
+var PORT = 4200;
+var path = require('path');
 
 //to control iRobot create2
-const SerialPort = require("serialport");
-const fs = require("fs");
-const Repl = require("repl");
-const Devices = require("./src/detect");
+var fs = require("fs");
+var Repl = require("repl");
+var Devices = require("./src/detect");
 
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const Roomba = require("./src/roombaController.js");
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var Roomba = require("./src/roombaController.js");
 
-Devices.getArduinoComName()
-  .then( port => {
-    board = new five.Board({
-      "repl": false,
-      port
-    });
-    board.on("ready", boardHandler);
-    board.on("fail", event => {
-      console.error(event);
-    });
+Devices.getArduinoComName().then(function (port) {
+  board = new five.Board({
+    "repl": false,
+    port: port
   });
+  board.on("ready", boardHandler);
+  board.on("fail", function (event) {
+    console.error(event);
+  });
+});
 
-
-app.get('/', (req, res) => {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'client/index.html'));
 });
 
 app.use("/", express.static(path.join(__dirname, 'client')));
 app.use("/assets", express.static(path.join(__dirname, 'client/assets')));
-http.listen(PORT, function(){
-  console.log('Listen on ',PORT);
+http.listen(PORT, function () {
+  console.log('Listen on ', PORT);
 });
 
-let pixel = require("node-pixel");
-let five = require("johnny-five");
+var pixel = require("node-pixel");
+var five = require("johnny-five");
 
 var strip = null;
 var numPixels = 12;
@@ -46,13 +45,12 @@ var numPixels = 12;
 var fps = 30; // how many frames per second do you want to try?
 
 //Connect to PSVR
-let yaw = 90;
-let servo_yaw = 90;
-
+var yaw = 90;
+var servo_yaw = 90;
 
 function boardHandler() {
   servo_yaw = new five.Servo({
-    pin : 6,
+    pin: 6,
     range: [30, 150],
     startAt: 90
   });
@@ -62,19 +60,19 @@ function boardHandler() {
   strip = new pixel.Strip({
     board: this,
     controller: "FIRMATA",
-    strips: [ {pin: 3, length: 12}, {pin: 7, length: 8},]
+    strips: [{ pin: 3, length: 12 }, { pin: 7, length: 8 }]
   });
 
-  strip.on("ready", function() {
+  strip.on("ready", function () {
 
     console.log("Strip ready");
 
     var colors = ["red", "green", "blue"];
-    var current_colors = [0,1,2];
-    var pixel_list = [0,1,2];
-    var blinker = setInterval(function() {
+    var current_colors = [0, 1, 2];
+    var pixel_list = [0, 1, 2];
+    var blinker = setInterval(function () {
       strip.color("#000"); // blanks it out
-      for (var i=0; i< pixel_list.length; i++) {
+      for (var i = 0; i < pixel_list.length; i++) {
         if (++pixel_list[i] >= strip.stripLength()) {
           pixel_list[i] = 0;
           if (++current_colors[i] >= colors.length) current_colors[i] = 0;
@@ -83,26 +81,23 @@ function boardHandler() {
       }
 
       strip.show();
-    }, 1000/fps);
+    }, 1000 / fps);
   });
 
-  strip.on("error", function(err) {
+  strip.on("error", function (err) {
     console.log(err);
     process.exit();
   });
-
 };
 
-Devices.getRoombaComName()
-  .then( port => {
-    const roomba = new Roomba(port);
-    io.of('/irobotCommand').on('connection', roomba.handler.bind(roomba));
-  });
-
-io.sockets.on('connection', function(socket) {
-  console.log("hello socket");
-  socket.on('servo', function(deg) {
-    if (deg[0] !== null && deg[0] !== undefined) servo_yaw.to(180 - deg[0]);
-  })
+Devices.getRoombaComName().then(function (port) {
+  var roomba = new Roomba(port);
+  io.of('/irobotCommand').on('connection', roomba.handler.bind(roomba));
 });
 
+io.sockets.on('connection', function (socket) {
+  console.log("hello socket");
+  socket.on('servo', function (deg) {
+    if (deg[0] !== null && deg[0] !== undefined) servo_yaw.to(180 - deg[0]);
+  });
+});
--- src/roombaController.js
+++ src/roombaController.js
@@ -1,91 +1,119 @@
-const SerialPort = require('serialport').SerialPort;
-const debug = require('debug')('create2:driver');
+'use strict';
 
-const RoombaOptions = {
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var SerialPort = require('serialport').SerialPort;
+var debug = require('debug')('create2:driver');
+
+var RoombaOptions = {
   baudRate: 115200,
   dataBits: 8,
-  parity: 'none', 
+  parity: 'none',
   stopBits: 1,
   flowControl: false
 };
 
-class Roomba {
+var Roomba = function () {
+  function Roomba(port) {
+    var _this = this;
 
-  constructor(port) {
-    
+    _classCallCheck(this, Roomba);
+
     this.inputBuffer = null;
     this.port = new SerialPort(port, RoombaOptions);
-    this.commands = [
-      () => { this.drive(-255, -255); },
-      () => { this.drive(255, 255); },
-      () => { this.drive(-80, 80); },
-      () => { this.drive(80, -80); },
-      () => { this.port.write(Buffer.from([7])); },
-      () => { this.port.write(Buffer.from([132])); },
-      () => { this.port.write(Buffer.from([131])); },
-      () => { this.port.write(Buffer.from([128])); },
-      () => { this.port.write(Buffer.from([139, 0, 125, 255])); },
-      () => { this.port.write(Buffer.from([139, 0, 0, 0])); },
-      () => { this.port.write(Buffer.from([140, 3, 1, 64, 16, 141, 3])); },
-      () => { this.close(); },
-      () => { this.drive(0,0); }
-    ];
+    this.commands = [function () {
+      _this.drive(-255, -255);
+    }, function () {
+      _this.drive(255, 255);
+    }, function () {
+      _this.drive(-80, 80);
+    }, function () {
+      _this.drive(80, -80);
+    }, function () {
+      _this.port.write(Buffer.from([7]));
+    }, function () {
+      _this.port.write(Buffer.from([132]));
+    }, function () {
+      _this.port.write(Buffer.from([131]));
+    }, function () {
+      _this.port.write(Buffer.from([128]));
+    }, function () {
+      _this.port.write(Buffer.from([139, 0, 125, 255]));
+    }, function () {
+      _this.port.write(Buffer.from([139, 0, 0, 0]));
+    }, function () {
+      _this.port.write(Buffer.from([140, 3, 1, 64, 16, 141, 3]));
+    }, function () {
+      _this.close();
+    }, function () {
+      _this.drive(0, 0);
+    }];
 
-    this.port.on('open', () => {
+    this.port.on('open', function () {
       debug('connected');
-      this.init();
+      _this.init();
     });
 
-    this.port.on('data', data => {
-      if (!this.inputBuffer) {
-        this.inputBuffer = data;
+    this.port.on('data', function (data) {
+      if (!_this.inputBuffer) {
+        _this.inputBuffer = data;
       } else {
-        this.inputBuffer = Buffer.concat([this.inputBuffer, data]);
+        _this.inputBuffer = Buffer.concat([_this.inputBuffer, data]);
       }
 
-      debug(`${this.inputBuffer.length} Bytes received`);
-      debug(this.inputBuffer.toJSON().data.toString());
+      debug(_this.inputBuffer.length + ' Bytes received');
+      debug(_this.inputBuffer.toJSON().data.toString());
 
-      if (this.inputBuffer.length == 80) {
+      if (_this.inputBuffer.length == 80) {
         debug('===== Clear');
-        this.inputBuffer = null;
-      }
-    });
-
-  }
-
-  init() {
-    this.port.write(Buffer.from([128]));
-    this.port.write(Buffer.from([131]));
-    this.port.write(Buffer.from([7]));
-  }
-
-  close () {
-    debug('close');
-    this.port.close();
-    process.exit(0);
-  }
-
-
-  drive (right, left) {
-    const buf = new Buffer(5);
-    buf[0] = 145;
-    buf.writeInt16BE(right, 1);
-    buf.writeInt16BE(left, 3);
-    this.port.write(buf);
-    debug(buf.toJSON().data.toString());
-  }
-
-  handler(socket) {
-    socket.on('message', data => {
-      debug(data);
-      if (this.port.isOpen) {
-        this.commands[data.id]();
-      } else {
-        console.error('port is not open');
+        _this.inputBuffer = null;
       }
     });
   }
-}
+
+  _createClass(Roomba, [{
+    key: 'init',
+    value: function init() {
+      this.port.write(Buffer.from([128]));
+      this.port.write(Buffer.from([131]));
+      this.port.write(Buffer.from([7]));
+    }
+  }, {
+    key: 'close',
+    value: function close() {
+      debug('close');
+      this.port.close();
+      process.exit(0);
+    }
+  }, {
+    key: 'drive',
+    value: function drive(right, left) {
+      var buf = new Buffer(5);
+      buf[0] = 145;
+      buf.writeInt16BE(right, 1);
+      buf.writeInt16BE(left, 3);
+      this.port.write(buf);
+      debug(buf.toJSON().data.toString());
+    }
+  }, {
+    key: 'handler',
+    value: function handler(socket) {
+      var _this2 = this;
+
+      socket.on('message', function (data) {
+        debug(data);
+        if (_this2.port.isOpen) {
+          _this2.commands[data.id]();
+        } else {
+          console.error('port is not open');
+        }
+      });
+    }
+  }]);
+
+  return Roomba;
+}();
 
 module.exports = Roomba;
